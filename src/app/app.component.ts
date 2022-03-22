6c2918be973a4ac7ab74e33ca009660e2da97f7e
Marvin Delroy Aroza <57531814+marvin-aroza@users.noreply.github.com>
2022-03-22 17:41:55 +0530

    config changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -33,6 +33,8 @@ export class AppComponent implements OnDestroy {
       fr: 'F',
       sa: 'S',
     },
+    firstDayOfWeek: 'su',
+    markCurrentMonth: true
   };
 
   model: IMyDateModel | null = null;
